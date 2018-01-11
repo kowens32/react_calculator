7f47840de3f862386970725a2b95a49e21d3336a
Kimberly <kowens4@elon.edu>
2018-01-11 18:28:37 -0500

    setup webpack loaders configuration

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
-import React from 'react'; //import React package
-import ReactDOM from 'react-dom'; //import ReactDom Package
+import React from 'react'; // import React package
+import ReactDOM from 'react-dom'; // import ReactDOM package
+import './styles/main.css'; // import our external css file
 
-//using the render method, we will mount this node into our DOM (html file) on the element with
-//id of 'app'
-
+// using the render method, we will mount this node into our DOM (html file) on the element with
+// id of 'app'
 ReactDOM.render(
-    <div>Hello World</div>,
-document.getElementById('app')
+    <div> Hello World</div>,
+    document.getElementById('app')
 );
